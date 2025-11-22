7b269652667d1447c40b8999fe298ef6891807cd
baticlean <baticlean225@gmail.com>
2025-11-22 00:42:20 +0000

    Coreection

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,13 @@
+// kevyamon/lokolearn_backend/LokoLearn_Backend-80d946f165c0cfa3aca77a220fc2a35a52f497cd/config/cloudinary.js
 const cloudinary = require('cloudinary').v2;
+const dotenv = require('dotenv');
 
-const configureCloudinary = () => {
-  cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
-};
+dotenv.config();
 
-module.exports = configureCloudinary;
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+module.exports = cloudinary;
