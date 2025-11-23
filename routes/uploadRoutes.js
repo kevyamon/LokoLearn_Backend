1cc57eddc974584e97c56b0a01dd2e82f056ebaa
baticlean <baticlean225@gmail.com>
2025-11-23 16:36:57 +0000

    Correction PAge cours

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,3 +1,4 @@
+// kevyamon/lokolearn_backend/LokoLearn_Backend-80d946f165c0cfa3aca77a220fc2a35a52f497cd/routes/uploadRoutes.js
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
@@ -10,20 +11,20 @@ const {
 
 const { protect, professor } = require('../middleware/authMiddleware');
 
-// Config Multer
+// Config Multer (Mémoire tampon)
 const storage = multer.memoryStorage();
 const upload = multer({ 
   storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 } // 10 MB max
+  limits: { fileSize: 15 * 1024 * 1024 } // Augmenté à 15 MB pour les gros PPT
 });
 
 // --- ROUTES ---
 
 // 1. Upload Cours (Prof seulement)
+// On accepte tout type de fichier ici, la validation se fera dans le contrôleur ou Cloudinary
 router.post('/', protect, professor, upload.single('file'), uploadFile);
 
-// 2. Gestion Bannière (Admin ou Prof, à ajuster)
-// Note: getBannerImages est public pour que la page d'accueil l'affiche
+// 2. Gestion Bannière
 router.get('/banner', getBannerImages);
 router.post('/banner', protect, professor, upload.single('image'), uploadBannerImage);
 router.delete('/banner/:id', protect, professor, deleteBannerImage);
